5a7dfae75932314f8d177e94b62b5f0e064dafef
chisomOx <onyenankiekelvin@gmail.com>
2024-01-09 01:02:15 +0100

    modified:   .gitignore 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	new file:   s3Service.js 	new file:   uploads/[object Object]-Onyenankie_Chisom_Kelvin_Admission_Letter.PDF 	deleted:    uploads/[object Object]-Screenshot 2024-01-04 234538.png 	deleted:    uploads/[object Object]-Screenshot 2024-01-04 234552.png

--- index.js
+++ index.js
@@ -1,7 +1,70 @@
+ require("dotenv").config()
 const express = require('express')
 const multer = require('multer')
-const uuid = require('uuid')
+const { s3Uploadv2, s3Uploadv3 } = require("./s3Service")
 const app = express()
+
+const storage = multer.memoryStorage()
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype.split("/")[0] === "image"){
+        cb(null, true)
+    } else {
+        cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false)
+    }
+}
+
+ const upload = multer({
+    storage,
+    fileFilter,
+     // limits: { fileSize: 10000000, files: 2}
+})
+
+// uploading with aws s3 version 2
+//  app.post('/upload', upload.array('image'), async(req, res) => {
+//    try{
+//     const results = await s3Uploadv2(req.files)
+//     console.log(results)
+//     res.json({status: 'success'})
+//    } catch (err) {
+//     res.json({status: 'failed to upload image', })
+//      console.log(err)
+//    }
+//  })
+
+// uploading with aws s3 wersion 3
+app.post('/upload', upload.array('image'), async(req, res) => {
+    try{
+     const results = await s3Uploadv3(req.files)
+     console.log(results)
+     res.json({status: 'success'})
+    } catch (err) {
+     res.json({status: 'failed to upload image', })
+      console.log(err)
+    }
+  })
+
+ app.use((error, req, res, next) => {
+    if(error instanceof multer.MulterError) {
+        if (error.code == "LIMIT_FILE_SIZE") {
+          return  res.json({message: 'file size too big'})
+        }
+        if(error.code == "LIMIT_FILE_COUNT"){
+          return  res.json({message: 'file limit reached'}) 
+        }
+        if(error.code == "LIMIT_UNEXPECTED_FILE"){
+            return  res.json({message: 'file MUST be of image type'}) 
+          }
+    }
+ })
+
+app.listen(3000, () => console.log('listening on port 3000'))
+
+
+
+
+// UPLOADING IMAGES TO NODE SERVER USING MULTER
+
 
 // const upload = multer({dest: 'uploads/'})
 // app.post('/upload', upload.array('image'), (req, res) => {
@@ -16,31 +79,12 @@ const app = express()
 //     res.json({status: 'success'})
 // })
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads");
-    },
-    filename: (req, file, cb) => {
-        const {originalname} = file;
-        cb(null, `${uuid}-${originalname}` )
-    }
-})
-
-// const fileFilter = (req, file, cb) => {
-//     if(file.mimetype.split("/")[0] === 'image/png'){
-//         cb(null, true)
-//     } else {
-//         cb(new Error("the file is not correct"))
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, "uploads");
+//     },
+//     filename: (req, file, cb) => {
+//         const {originalname} = file;
+//         cb(null, `${uuid}-${originalname}` )
 //     }
-// }
-
- const upload = multer({
-    storage,
-   //  fileFilter,
-     limits: { fileSize: 1000000000, files: 2}
-})
- app.post('/upload', upload.array('image'), (req, res) => {
-     res.json({status: 'success'})
- })
-
-app.listen(3000, () => console.log('listening on port 3000'))
+// })
